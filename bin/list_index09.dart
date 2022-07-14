06695b5b618bbb4e08ca9209dd9d5bef8bce1c83
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 10:32:37 +0500

    list_index09.dart

--- bin/list_index09.dart
+++ bin/list_index09.dart
@@ -6,4 +6,18 @@
     Returns:
         bool: return answer
 */
-void main() {}
+bool func(list1){
+  int h=0;
+  bool n=true;
+  List d=[];
+  while(h<=list1.length-1){
+    if(list1[h]==list1[list1.length-1]){
+      d.add(true);
+    }h++;
+  }if(d.length==list1.length){
+    n=true;
+
+  }else n=false;
+  return n;
+}
+void main() {print(func(['go','went','gone']));}
