a3cc7fac06558fa6914158babd6217f3698c4cbd
saloh7856 <salohiddinberdiyorov5@gmail.com>
2022-07-14 08:25:05 +0500

    list_index06.dart

--- bin/list_index06.dart
+++ bin/list_index06.dart
@@ -6,4 +6,16 @@
     Returns:
         list: return answer
 */
-void main() {}
+
+List func(List list1){
+    int i = 0;
+    while(list1[i] == 1){
+        list1[i] = 'True';
+        i += 1;
+        
+    }
+    return list1;
+}
+void main(){
+    print(func([1,0,0,0,0]));
+}
